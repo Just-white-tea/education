246a74b4310e5b7bdd99de2f6daad179186c6349
java-gj <389146770@qq.com>
2019-09-20 14:18:54 +0800

    day 2

--- page/src/main/java/com/vip/page/PageApplication.java
+++ page/src/main/java/com/vip/page/PageApplication.java
@@ -1,9 +1,11 @@
 package com.vip.page;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.vip.page.mapper")
 public class PageApplication {
 
     public static void main(String[] args) {
