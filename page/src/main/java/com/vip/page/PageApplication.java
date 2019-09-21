3ebae233d69f4a2c6e73a170b88c6ed096d44f27
java-gj <389146770@qq.com>
2019-09-21 15:58:58 +0800

    day 2

--- page/src/main/java/com/vip/page/PageApplication.java
+++ page/src/main/java/com/vip/page/PageApplication.java
@@ -1,8 +1,10 @@
 package com.vip.page;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 @MapperScan("com.vip.page.mapper")
